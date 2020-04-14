a65322b57f7fb7f4986aae88a7faaaf8ff36e0b7
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-04-14 16:26:49 -0400

    feat: craeted func to read files

--- lib/share.ex
+++ lib/share.ex
@@ -1,18 +1,31 @@
 defmodule Share do
   @moduledoc """
-  Documentation for Share.
+  Share module contain all function to interactive inside of node.
   """
 
   @doc """
-  Hello world.
+  Function to read filename...
 
   ## Examples
 
-      iex> Share.hello()
-      :world
+      iex> Share.file_to_share
+      test.txt
+      %{data: "No found!", path: "test.txt"}
 
   """
-  def hello do
-    :world
+  def file_to_share do
+    IO.puts "Path of filename: "
+    IO.read(:line)
+    |> String.trim
+    |> Share.do_file_to_share
+  end
+
+  def do_file_to_share(filename) do
+    case File.read(filename) do
+      {:ok, data} ->
+        %{path: filename, data: data}
+      {:error, :enoent} ->
+        %{path: filename, data: "No found!"}
+    end
   end
 end
