a2f5b0c5a3bf2f5a253a5304ac550c8b29204c81
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-04-14 14:15:58 -0400

    feat: init project.

---
+++ lib/share.ex
@@ -0,0 +1,18 @@
+defmodule Share do
+  @moduledoc """
+  Documentation for Share.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Share.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lib/share/application.ex
@@ -0,0 +1,19 @@
+defmodule Share.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Share.Worker.start_link(arg)
+      # {Share.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Share.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
